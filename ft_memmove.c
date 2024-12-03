960c6b2f86c45541d2591183d8cfe324f2b8089e
Kragline <armanaraqelyan1107@gmail.com>
2024-12-03 18:26:08 +0400

    modified ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,25 +14,17 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	unsigned int	i;
 	unsigned char	*d_ptr;
 	unsigned char	*s_ptr;
 
 	if (!dest && !src)
 		return (NULL);
-	i = 0;
 	d_ptr = (unsigned char *)dest;
 	s_ptr = (unsigned char *)src;
 	if (dest > src)
 		while (n-- > 0)
 			d_ptr[n] = s_ptr[n];
 	else
-	{
-		while (i < n)
-		{
-			d_ptr[i] = s_ptr[i];
-			i++;
-		}
-	}
+		ft_memcpy(dest, src, n);
 	return (dest);
 }
