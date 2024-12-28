976297b90fd1567393c53a5c0ec253d6f1d146a5
Kragline <armanaraqelyan1107@gmail.com>
2024-12-28 21:23:27 +0400

    Makefile and some changes in ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -12,13 +12,6 @@
 
 #include "libft.h"
 
-static size_t	is_our_char(char const c1, char c2)
-{
-	if (c1 == c2)
-		return (1);
-	return (0);
-}
-
 static size_t	count_words(char const *str, char c)
 {
 	size_t	count;
@@ -28,12 +21,12 @@ static size_t	count_words(char const *str, char c)
 	inword = 0;
 	while (*str)
 	{
-		if (!is_our_char(*str, c) && inword == 0)
+		if (*str != c && inword == 0)
 		{
 			inword = 1;
 			count++;
 		}
-		else if (is_our_char(*str, c) && inword == 1)
+		else if (*str != c && inword == 1)
 			inword = 0;
 		str++;
 	}
@@ -46,42 +39,63 @@ static char	*malloc_word(char const *str, char c, size_t *index)
 	char	*word;
 
 	i = 0;
-	while (str[i] && !is_our_char(str[i], c))
+	while (str[i] && str[i] != c)
 		i++;
 	word = malloc(sizeof(char) * (i + 1));
 	if (!word)
 		return (NULL);
 	i = 0;
-	while (str[*index] && !is_our_char(str[*index], c))
+	while (str[*index] && str[*index] != c)
 	{
 		word[i] = str[*index];
 		(*index)++;
 		i++;
 	}
 	word[i] = '\0';
-	while (is_our_char(str[*index], c))
+	while (str[*index] == c)
 		(*index)++;
 	return (word);
+}
+
+static void	free_array(char **array)
+{
+	int	i;
+
+	i = 0;
+	while (array[i])
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
+
+static char	**initialize_vars(char const *s, char c, size_t *i, size_t *j)
+{
+	*i = 0;
+	*j = 0;
+	return (malloc(sizeof(char *) * (count_words(s, c) + 1)));
 }
 
 char	**ft_split(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
-	size_t	arrlen;
 	char	**arr;
 
-	arrlen = count_words(s, c);
-	arr = malloc(sizeof(char *) * (arrlen + 1));
+	arr = initialize_vars(s, c, &i, &j);
 	if (!arr)
 		return (NULL);
-	i = 0;
-	j = 0;
 	while (s[i])
 	{
-		if (!is_our_char(s[i], c))
+		if (s[i] != c)
 		{
 			arr[j] = malloc_word(s, c, &i);
+			if (!arr[j])
+			{
+				free_array(arr);
+				return (NULL);
+			}
 			j++;
 		}
 		else
